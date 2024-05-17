2ae2ee6c22279cd0c7c51d29eacc0c54b3e0f89b
joelezz <joel.ezzahid@gmail.com>
2024-05-17 21:00:50 +0300

    Added print_customers function and some comments aswell to add some readability to the code.

--- crm.py
+++ crm.py
@@ -3,18 +3,23 @@
 customers = []
 
 def crm():
+    #Welcoming the user
     def welcome_message():
-        print("Hello! And welcome to the business CRM 1.0")
+        print("Hello! And welcome to the Business CRM 1.0")
     welcome_message()
+    
     while True:
-        options = input("1.add customer 2. print customers or enter + blank to end the program ")
+        options = input("Enter an option: 1.Add a customer 2. Print customer list (Press ENTER without any option to end the program ")
         if options == "":
+            #Politely thanking the user for using the software at the end of the session
+            print("Thank you for using the Business CRM 1.0")
             break
         elif options == "1":
-                add_customer()        
+            add_customer()        
         elif options == "2":
-                print(customers)
+            print_customers()
 
+#The function to handle the logic with adding of a new customer
 def add_customer():      
     name = input("name of your customer: ")
     email = input("email of your customer: ")
@@ -22,7 +27,16 @@ def add_customer():
     customers.append(customer)
 
     print(f"{customer} has been added to your database.")
-    print(f"Updated db") 
+    print(f"Updated database") 
+
+def print_customers():
+    if customers:
+        print("Customers list:")
+        for customer in customers:
+            print(f"Name: {customer['name']}, Email: {customer['email']}")
+    else:
+        print("No customers in the database.")
+
 
 if __name__ == "__main__":
     crm()
